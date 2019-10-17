7fe025c8dafc12a0f5d59488761e915621fccf63
Tejas Shah <tejas4197@hotmail.com>
2019-10-17 12:06:50 -0400

    Added frame counter on game  canvas for profiling purposes and future testing. Press F12 to enable and disable frame counter.

---
+++ Assets/_RESILIENCE/Scripts/Utility/FrameCounter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FrameCounter : MonoBehaviour
+{
+    private int frameRate;
+    public Text framesCounterText;
+
+    public void LateUpdate()
+    {
+        GetFrames();
+    }
+
+    private void GetFrames()
+    {
+        if (Input.GetKeyDown(KeyCode.F12))
+        {
+            if (framesCounterText.gameObject.activeSelf == false)
+            {
+                framesCounterText.gameObject.SetActive(true);
+                float current = 0;
+                current = Time.frameCount / Time.time;
+                frameRate = Mathf.RoundToInt(current);
+                framesCounterText.text = frameRate.ToString() + " FPS";
+            }
+            else if(framesCounterText.gameObject.activeSelf == true)
+            {
+                framesCounterText.gameObject.SetActive(false);
+            }
+        }
+    }
+}
