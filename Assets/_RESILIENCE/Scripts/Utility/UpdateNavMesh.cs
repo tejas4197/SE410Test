d875e2745c896634e55751a7a666fa33ea194756
Tejas Shah <tejas4197@hotmail.com>
2019-10-19 14:09:55 -0400

    added obstacle to building var01 and a basic update/clear function for surface testing. BUG: unity crashes

---
+++ Assets/_RESILIENCE/Scripts/Utility/UpdateNavMesh.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UpdateNavMesh : MonoBehaviour
+{
+    public NavMeshSurface meshSurface;
+
+    private void Update()
+    {
+        BakeAndClear();
+    }
+    void BakeAndClear()
+    {
+        if (Input.GetKeyDown(KeyCode.F10))
+        {
+            meshSurface.RemoveData();
+        }
+        else if (Input.GetKeyDown(KeyCode.F11))
+        {
+            meshSurface.BuildNavMesh();
+        }
+    }
+    
+}
