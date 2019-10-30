204c6ec5d3482931651f0adae00ef880725f7a8b
LilyLauben <31350278+LilyLauben@users.noreply.github.com>
2019-10-29 22:01:45 -0400

    Created simple tutorial popup
    just a single image panel, a script on it that pauses the game on start and that unpauses and destroys the panel if any key is pressed

---
+++ Assets/_RESILIENCE/Scripts/UI/TutorialPopupPause.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialPopupPause : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeManager.GetInstance().SetTimePassageMultiplier(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey) { // not using rewired because this is too simple to need it
+            TimeManager.GetInstance().SetTimePassageMultiplier(1);
+            Destroy(this.gameObject);
+        }
+    }
+}
